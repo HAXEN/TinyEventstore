1f75d7a11b22d54d2a19b5e998a4caf7d440c5ec
Håkan Alexander <hakan.alexander@gmail.com>
2017-05-12 15:38:46 +0200

    Test in progress

--- TinyEventstore.Consumer/PollingClient.cs
+++ TinyEventstore.Consumer/PollingClient.cs
@@ -1,6 +1,8 @@
 using System;
+using System.Collections.Generic;
 using System.Reactive;
-using System.Reactive.Subjects;
+using System.Reactive.Concurrency;
+using System.Reactive.Linq;
 using System.Threading.Tasks;
 
 namespace TinyEventstore.Consumer
@@ -26,15 +28,50 @@ namespace TinyEventstore.Consumer
             private readonly string _connectionString;
             private readonly int _interval;
             private readonly int _offset;
-            //private readonly ISubject<IPersistedEventData> _subject = Subject.;
+            private readonly IObservable<IPersistedEventData> _events;
+            private readonly IScheduler _scheduler;
 
             public PollingObserver(string connectionString, int interval, int offset)
             {
                 _connectionString = connectionString;
                 _interval = interval;
                 _offset = offset;
+
+                _scheduler = new EventLoopScheduler();
+                
+                _events = SetupListener();
+
+
             }
 
+            private IObservable<IPersistedEventData> SetupListener()
+            {
+                return Observable.Create<IPersistedEventData>(o =>
+                    {
+                        return _scheduler.Schedule(TimeSpan.FromMilliseconds(_interval), (interval, recurse) =>
+                        {
+                            try
+                            {
+                                var events = PollEvents();
+                                foreach (var @event in events)
+                                {
+                                    o.OnNext(@event);
+                                }
+                                recurse(interval);
+                            }
+                            catch (Exception exception)
+                            {
+                                Console.WriteLine(exception);
+                                o.OnError(exception);
+                            }
+                        });
+                    });
+            }
+
+            private IEnumerable<IPersistedEventData> PollEvents()
+            {
+                yield return null;
+            }
 
             public Task Start()
             {
